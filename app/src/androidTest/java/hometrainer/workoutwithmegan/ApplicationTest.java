b131d79849467d2278d6877d24660cf5ad6eb13a
mgnmcd <meganmcd0nald@yahoo.com>
2016-03-28 23:17:39 -0400

    Initial commit

---
+++ app/src/androidTest/java/hometrainer/workoutwithmegan/ApplicationTest.java
@@ -0,0 +1,13 @@
+package hometrainer.workoutwithmegan;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
